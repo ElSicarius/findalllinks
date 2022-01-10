059a91eca92cd9422c6ede84d40c69591883f3e8
sicarius <amurat654@gmail.com>
2022-01-10 13:33:45 +0100

    working tool for absolute urls

--- 0.1/content.js
+++ 0.1/content.js
@@ -1,22 +1,36 @@
 
 var find_stuff = function(){
+	var stuff = "";
 	function printScriptTextContent(script) {
    var xhr = new XMLHttpRequest();
-   xhr.open("GET", script.src)
-   xhr.onreadystatechange = function() {
-      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-         return xhr.responseText;
-      }
-   };
-   xhr.send();
+	 var stuf = "";
+	 console.log(script.src)
+   xhr.open("GET", script.src, false)
+	 try {
+   	xhr.send( null );
+	} catch (error){
+		return ""
 	}
-	return Array.prototype.slice.call(document.querySelectorAll("script[src]")).forEach(printScriptTextContent);
+	 return xhr.responseText;
+	}
+	var scripts = document.querySelectorAll("script[src]");
+	console.log(scripts.length);
+	for (var i = 0; i < scripts.length; i++){
+			stuff += printScriptTextContent(scripts[i]);
+	}
+	const regex_links = /https?:[\\\/]*[\w\_\.\:\d\-]+([\\\/]*)([\\\/\w\_\-\d]*)(\?([^&'},)"])([^=]+=[^&'},)"]+)?)?/gmi;
+	const relative = /[A-Z]/g;
+	console.log("Regex link");
+	console.log(new Set(stuff.match(regex_links)));
+
+
 }
 
 
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	console.log("Command "+request.command+" Recieved");
 	if(request.command === "find"){
-		return find_stuff();
+		find_stuff();
 	}
 });
--- 0.1/trouvereur.js
+++ 0.1/trouvereur.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function() {
 	document.querySelector('#all').addEventListener('click', function() {
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 			chrome.tabs.sendMessage(tabs[0].id, {command: "find"}, function(response) {
-				console.log(response);
+				console.log("Call to function");
 			});
 		});
 	});
